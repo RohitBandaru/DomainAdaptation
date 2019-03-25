285bd44e37305814d157d69a219a6050bbcac661
Arun Pidugu <apidugu@gmail.com>
2019-03-25 18:23:47 -0400

    changes to i2i-adapt

--- i2i-adapt.py
+++ i2i-adapt.py
@@ -8,6 +8,15 @@ import torch.optim as optim
 from torch.autograd import Variable
 hidden_dims = 500
 
+weights = {
+    "c": 1.0,
+    "z": 0.05,
+    "id": 0.1,
+    "tr": 0.02,
+    "cyc": 0.1,
+    "trc": 0.1,
+}
+
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
@@ -116,6 +125,11 @@ def test(encoder, classifier, test_loader, source=True):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset))),
 
+
+def qid_loss(source_encoding, class_classifier, data_s, target_s):
+    output = class_classifier(source_encoding)
+    return F.nll_loss(output, target_s)
+
 def train(source_encoder, source_decoder, target_encoder, target_decoder,
     class_classifier, domain_classifier, source_discriminator, target_discriminator,
     source_train_loader, target_train_loader, source_test_loader, target_test_loader,
@@ -140,56 +154,82 @@ def train(source_encoder, source_decoder, target_encoder, target_decoder,
 
         data_zip = zip(target_train_loader, source_train_loader)
         for batch_idx, ((data_t, _), (data_s, target_s)) in enumerate(data_zip):
-            # 1 source classification
             source_encoding = source_encoder(data_s)
-            output = class_classifier(source_encoding)
-
-            c_loss = F.nll_loss(output, target_s)
-
-            # 2 decoder
             source_decoding = source_decoder(source_encoding)
+
             target_encoding = target_encoder(data_t)
             target_decoding = target_decoder(target_encoding)
 
-            id_loss = nn.MSELoss()(data_s, source_decoding) + nn.MSELoss()(data_t, target_decoding)
-
-            # 3 domain classification loss
             source_domain = domain_classifier(source_encoding)
             target_domain = domain_classifier(target_encoding)
 
-            z_out = torch.cat((Variable(source_domain), Variable(target_domain)),0)
-            z_label = torch.cat((Variable(torch.ones(source_domain.size()[0]).long()),
-                             Variable(torch.zeros(target_domain.size()[0]).long())),0)
-            z_loss = F.nll_loss(z_out.float().requires_grad_(), z_label)
-
-            # 4 translation discriminate
             source_translation = target_decoder(source_encoding)
             target_translation = source_decoder(target_encoding)
-
             source_translation_d = target_discriminator(source_translation)
             target_translation_d = source_discriminator(target_translation)
 
-            tr_out = torch.cat((Variable(source_translation_d), Variable(target_translation_d)),0)
-            tr_label = torch.cat((Variable(torch.ones(source_translation_d.size()[0]).long()),
-                             Variable(torch.zeros(target_translation_d .size()[0]).long())),0)
-            tr_loss = F.nll_loss(tr_out.float().requires_grad_(), tr_label)
-
-            # 5 Cycle consistency
             source_target_encoding = target_encoder(source_translation)
             source_cycle = source_decoder(source_target_encoding)
             target_cycle = target_decoder(source_encoder(target_translation))
 
-            cyc_loss = nn.MSELoss()(data_s, source_cycle) + nn.MSELoss()(data_t, target_cycle)
+            # 1 source classification
+            c_loss = weights["c"] * qid_loss(source_encoding, class_classifier, data_s, target_s)
+
+            # 2 decoder
+            id_loss = weights["id"] * nn.MSELoss()(data_s, source_decoding) + nn.MSELoss()(data_t, target_decoding)
+
+            # 3 domain classification loss
+            z_out = torch.cat((Variable(source_domain), Variable(target_domain)),0)
+            z_label = torch.cat((Variable(torch.ones(source_domain.size()[0]).long()),
+                             Variable(torch.zeros(target_domain.size()[0]).long())),0)
+            z_loss = weights["z"] * F.nll_loss(z_out.float().requires_grad_(), z_label)
+
+            # 4 translation discriminate
+            tr_out = torch.cat((Variable(source_translation_d), Variable(target_translation_d)),0)
+            tr_label = torch.cat((Variable(torch.ones(source_translation_d.size()[0]).long()),
+                             Variable(torch.zeros(target_translation_d .size()[0]).long())),0)
+            tr_loss = weights["tr"] * F.nll_loss(tr_out.float().requires_grad_(), tr_label)
+
+            # 5 Cycle consistency
+            cyc_loss = weights["cyc"] * nn.MSELoss()(data_s, source_cycle) + nn.MSELoss()(data_t, target_cycle)
 
             # 6 Source to target classification loss
             output_trc = class_classifier(source_target_encoding)
-            trc_loss = F.nll_loss(output_trc, target_s)
+            trc_loss = weights["trc"] * F.nll_loss(output_trc, target_s)
 
             loss = c_loss + id_loss + z_loss + tr_loss + cyc_loss + trc_loss
             loss.backward()
             optimizer.step()
 
     return source_encoder, source_decoder, target_encoder, target_decoder, class_classifier, domain_classifier
+
+def load_mnist():
+    transform = transforms.Compose([
+                            transforms.Resize(size=(32,32)),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5,), (0.5,)),
+                            ])
+    trainset = datasets.MNIST('data/MNIST/', download=True, train=True, transform=transform)
+    testset = datasets.MNIST('data/MNIST/', download=True, train=False, transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=True)
+
+    return trainloader, testloader
+
+def load_svhn():
+    transform = transforms.Compose([
+                            transforms.Grayscale(),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5,), (0.5,)),
+                            ])
+    trainset = datasets.SVHN('data/SVHN/', download=True, split="train", transform=transform)
+    testset = datasets.SVHN('data/SVHN/', download=True, split="test", transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=True)
+
+    return trainloader, testloader
 
 if __name__ == '__main__':
     source_encoder = Encoder()
@@ -203,6 +243,9 @@ if __name__ == '__main__':
 
     source_discriminator = Discriminator()
     target_discriminator = Discriminator()
+
+    mnist_tr, mnist_te = load_mnist()
+    mnist_te, usps_te = load_svhn()
 
     source_encoder, source_decoder, target_encoder, target_decoder, class_classifier, domain_classifier = train(source_encoder, source_decoder, target_encoder, target_decoder,
         class_classifier, domain_classifier, source_discriminator, target_discriminator,
